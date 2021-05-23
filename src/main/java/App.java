33b7c2c9c854eeeb8a33dce3b811360ad79600c1
RazakovDamir <razakovda@yandex.ru>
2021-05-23 18:31:32 +0300

    2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,17 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld b = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(b.getMessage());
+
+        Cat catB = (Cat)applicationContext.getBean("cat");
+        System.out.println(catB.getMessage());
+
+        Cat catB1 = (Cat)applicationContext.getBean("cat");
+        System.out.println(catB1.getMessage());
+
+        System.out.println(bean.equals(b));
+        System.out.println(catB.equals(catB1));
     }
 }
